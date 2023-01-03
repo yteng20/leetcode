d42047b598bc5a1901761daaa6926caa625d290d
yteng20 <60151452+yteng20@users.noreply.github.com>
2023-01-02 19:02:24 -0800

    Update 26RemoveDuplicatesfromSortedArray.java

--- 26RemoveDuplicatesfromSortedArray.java
+++ 26RemoveDuplicatesfromSortedArray.java
@@ -14,6 +14,13 @@ assert k == expectedNums.length;
 for (int i = 0; i < k; i++) {
     assert nums[i] == expectedNums[i];
 }
+
+
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
 */
 
 
